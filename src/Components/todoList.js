09419c44a8d30020802e5362a845f3d604040898
rukkiechovwe <rukkiechowe@gmail.com>
2021-05-24 11:00:07 +0100

    workkking

--- src/Components/todoList.js
+++ src/Components/todoList.js
@@ -15,22 +15,7 @@ function TodoList({ item }) {
     >
       <div className="row al-cen-between fw">
         <div className="row al-cen-between">
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="i-size"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg> */}
-          <input type="checkbox" onClick={() => context.completedItem(item.id)}/>
+          <div><input type="checkbox" onClick={() => context.completedItem(item.id)}/></div>
           <span
             className="title"
             style={{
